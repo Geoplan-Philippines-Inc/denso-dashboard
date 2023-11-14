7bfea1538f413a0e4b93da2488624ad9f492b5ad
Ace Gabriel P. Pasiliao <150634534+geo-ace@users.noreply.github.com>
2023-11-14 13:36:04 +0800

    change: for production link

--- src/app/maintenance/service/user-master/user-master.service.ts
+++ src/app/maintenance/service/user-master/user-master.service.ts
@@ -10,7 +10,7 @@ export class UserMasterService {
     private http:HttpClient
   ) { }
 
-  readonly usersUrl = Constants.development.apiUrl.users;
+  readonly usersUrl = Constants.production.apiUrl.users;
 
   loadParts(){
     return this.http.get(this.usersUrl);
